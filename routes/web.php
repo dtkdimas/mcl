cc4951ae4fe32e2cb5e09ed989bd978c8f4602d4
Faiz <faqodefazz@gmail.com>
2024-03-21 10:37:09 +0700

    cmmt

--- routes/web.php
+++ routes/web.php
@@ -20,9 +20,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::group(['middleware' => 'guest'], function () {
-    // Route::get('/', function () {
-    //     return view('welcome');
-    // });
     Route::resource('/', WelcomeController::class);
     Route::get('/register', [AuthController::class, 'register'])->name('register');
     Route::post('/register', [AuthController::class, 'registerPost'])->name('register');
