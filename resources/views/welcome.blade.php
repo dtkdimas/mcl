8a353e1142b6ca7fb9713cda3fe19cdacc19de1a
Faiz <faqodefazz@gmail.com>
2024-04-26 16:45:34 +0700

    back

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,7 +14,7 @@
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
     {{-- import css --}}
-    <link rel="stylesheet" href="{{ asset('css/welcome.css') }}?v2">
+    <link rel="stylesheet" href="{{ asset('css/welcome.css') }}">
 
 </head>
 
