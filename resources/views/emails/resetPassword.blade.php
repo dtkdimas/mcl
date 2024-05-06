5742d57cef0ed13ef764b9867f90adef733c6ace
Faiz <faqodefazz@gmail.com>
2024-05-06 16:31:36 +0700

    mail

--- resources/views/emails/resetPassword.blade.php
+++ resources/views/emails/resetPassword.blade.php
@@ -1,6 +1,5 @@
 <h4>Hello</b></h4>
 <br>
-<a href="{{ route('password.reset', ['token' => $token]) }}">Reset Password Here!</a>
-<h1 style="display: none;">@dd($token)</h1>
+<a href="{{ route('password.reset', $token) }}">Reset Password Here!</a>
 <br>
 <h4>Thank you,</h4>
