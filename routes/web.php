9ab8e964feb49fab210cd6b21c95f20d89ee957d
Faiz <magang.designer@detik.com>
2024-05-13 11:03:35 +0700

    deleting homeController

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Http\Controllers\AuthController;
-use App\Http\Controllers\HomeController;
 use App\Http\Controllers\YearController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ComponentController;
